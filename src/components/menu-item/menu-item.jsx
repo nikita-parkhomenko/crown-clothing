52a5ff63fe3edc5d1d697126a828332dc3e07938
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-01-29 21:18:02 +0200

    Added new font. Broken homepage into smaller components. And finished styling menu-item component.

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import './menu-item.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
+    <div className={`${size} menu-item`}>
+     <div className="background-image"
+        style={{
         backgroundImage: `url(${imageUrl})`
-    }}
-     className={`${size} menu-item`}>
+    }} />
         <div className="content">
-            <h1 className="title"> {title} </h1>
+            <h1 className="title"> {title.toUpperCase()} </h1>
             <span className="subtitle"> Buy Now </span>
         </div>
     </div>
