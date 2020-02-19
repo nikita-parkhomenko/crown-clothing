47edf815524f621f032306719d55107917e5f0d8
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-02-20 00:00:06 +0200

    implemented redux-persist librery to store our root reducer into the localStorage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <Provider store={ store } >
-        <App />
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>, 
     document.getElementById('root')
     );
